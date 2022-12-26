4de4e767e0ed6c4521b9ef1bcb52dd3a77755688
Francisco Ana <franciscopdana@gmail.com>
2022-12-26 22:02:34 +0000

    refactor loadAirports and loadAirlines

---
+++ DatabaseViewer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Francisco Ana on 26/12/2022.
+//
+
+#include "DatabaseViewer.h"
---
+++ DatabaseViewer.h
@@ -0,0 +1,14 @@
+//
+// Created by Francisco Ana on 26/12/2022.
+//
+
+#ifndef TRABALHO2_DATABASEVIEWER_H
+#define TRABALHO2_DATABASEVIEWER_H
+
+
+class DatabaseViewer {
+
+};
+
+
+#endif //TRABALHO2_DATABASEVIEWER_H
